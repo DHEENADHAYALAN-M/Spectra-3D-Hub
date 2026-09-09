1513ef1b5a37984bbe58bbcb47d3e9556b785cfd
DHEENADHAYALAN-M <dheena20022007@gmail.com>
2026-09-09 11:21:54 +0530

    Adjust founder image size and push further to the right

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -30,13 +30,13 @@ export function HeroSection() {
 
       {/* ===== CONTENT ===== */}
       <div className="relative z-10 lg:z-[60] container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-28 pb-12 lg:py-0">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:gap-8 items-center min-h-[calc(100vh-5rem)]">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:gap-8 items-center min-h-[calc(100vh-4rem)]">
           {/* LEFT: TEXT & ACTIONS */}
           <motion.div
             initial={{ opacity: 0, x: -40 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.9 }}
-            className="lg:col-span-6 text-left py-8 lg:py-16 relative z-10"
+            className="lg:col-span-7 text-left py-8 lg:py-16 relative z-10"
           >
             {/* BADGE */}
             <span className="inline-block mb-6 px-4 py-2 rounded-full border border-neon-cyan/40 bg-neon-cyan/10 text-neon-cyan text-sm backdrop-blur">
@@ -77,13 +77,13 @@ export function HeroSection() {
             </div>
           </motion.div>
 
-          {/* RIGHT: FOUNDER IMAGE (In front of navbar) */}
-          <div className="lg:col-span-6 flex justify-center lg:justify-end items-end self-end relative z-[60] pointer-events-none">
-            <div className="w-full flex justify-center lg:justify-end items-end">
+          {/* RIGHT: FOUNDER IMAGE (Right-aligned, proportional size, in front of navbar) */}
+          <div className="lg:col-span-5 flex justify-center lg:justify-end items-end self-end relative z-[60] pointer-events-none">
+            <div className="w-full flex justify-center lg:justify-end items-end lg:translate-x-4 xl:translate-x-8">
               <img
                 src="/founderpic.png"
                 alt="Spectra 3D Hub Founder"
-                className="w-full max-w-[550px] sm:max-w-[650px] lg:max-w-none h-auto lg:h-[92vh] object-contain object-bottom scale-100 lg:scale-120 xl:scale-130 origin-bottom select-none"
+                className="w-full max-w-[380px] sm:max-w-[460px] lg:max-w-[520px] xl:max-w-[580px] max-h-[82vh] object-contain object-bottom select-none"
               />
             </div>
           </div>
